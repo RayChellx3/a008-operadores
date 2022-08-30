d1553a0620ad86f871022a4602645a905df7b314
RayChellx3 <raychel.campos@gmail.com>
2022-08-30 14:52:52 -0300

    feita alteração com correção in sala

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,10 +1,14 @@
 // Construa um programa que:
 
 // a) Peça ao usuário que insira um número **par**
-let numero1 = prompt ("Insira um número par:")
+let numero = prompt ("Insira um número par:")
+numero = Number (numero)
+console.log("Número inserido:",numero)
 // b) Imprima no console **o resto da divisão** desse número por 2.
-const resultado1 = numero1 / 2
-console.log("O resto deste número dividido por 2 é: ", resultado1)
+let resto = numero%2
+let resultado = numero/2
+console.log("Resultado divisão:", resultado)
+console.log("O resto deste número dividido por 2 é: ", resto)
 
 // c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
 // Ele sempre faz a divisão do número por 2, quando feito 2/2= 1 e 0/2=0, até nos numeros negativos esta funcionando corretamente.
